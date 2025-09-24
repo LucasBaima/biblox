4bb8d628b472289d4d751fe2b9d0773fae118763
LucasBaima <jhonelucasbay_@outlook.com>
2025-09-24 19:28:59 -0300

    Definindo caminho para o template HTML

--- livros/views.py
+++ livros/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def pag_inicial(request):
+    return HttpResponse("Bem vindo a página inicial da biblox")
 
 # Create your views here.
