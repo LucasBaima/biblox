567fd3e7e025885bf1582dfcbb78f6eb1aef6580
LucasBaima <jhonelucasbay_@outlook.com>
2025-09-27 18:22:03 -0300

    Nova rota para edição + id do registro

--- livros/urls.py
+++ livros/urls.py
@@ -8,6 +8,7 @@ app_name = "livros"    #namespace para que o nome das rotas sejam reconhecidas"
 urlpatterns = [
     path("", views.home, name="home1"),
     path("cadastrar/", views.cadastrar_livro, name="cadastrar"), #Rota passada para o html dentro da tag
-    path("remover/<int:id>", views.remover_livro, name="remover")   #<int:id>  Informar qual item queremos remover .. id -> campo no banco de dados que representa o item
+    path("remover/<int:id>", views.remover_livro, name="remover"),   #<int:id>  Informar qual item queremos remover .. id -> campo no banco de dados que representa o item
+    path("editar/<int:id>", views.editar_livro, name="editar") 
 ]
 
