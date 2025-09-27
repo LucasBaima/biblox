6377e480e47c4342146cf519dfe46747473a689f
LucasBaima <jhonelucasbay_@outlook.com>
2025-09-27 12:38:37 -0300

    Criação de um formulário a partir de um modelo existente

---
+++ livros/forms.py
@@ -0,0 +1,18 @@
+from django import forms
+from .models import CadastroLivroModel
+
+#Django possui uma capacidade implícita para criação de formulários!
+
+class CadastroForm(forms.ModelForm):
+    class Meta:  #class de configuração
+        model = CadastroLivroModel
+        fields = ["nome", "autor", "isbn", "completo"]                       #Quais campos do model serão considerados --> No Html
+        
+        
+    
+    
+    
+    #ModelForm -> Interface de criação de formulário (Formulário automático a partir de um modelo(models.py)) 
+
+
+
