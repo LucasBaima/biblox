65f854fca4e8352d51bd24da2dbf19b14769ccfc
LucasBaima <jhonelucasbay_@outlook.com>
2025-09-27 12:40:35 -0300

    Tratamento da entrada do usuário(request) após envia-lo(POST)

--- livros/views.py
+++ livros/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from .forms import CadastroForm #-> Importação do nosso formulário
+from django.http import HttpRequest
 
 
 def home(request): 
@@ -8,5 +9,14 @@ def home(request):
     }
     return render(request, "crud/inicial.html", user)
   
-def cadastrar_livro(request):
-    return HttpResponse("Cadastre o seu Livro aqui")
+def cadastrar_livro(request:HttpRequest):
+    if request.method == "POST":
+        formular = CadastroForm(request.POST) #Criando uma instância do nosso formulário que contenha as informações que enviamos p ele
+        if formular.is_valid():
+            formular.save()  #Salvar as informações no banco de dados
+            return redirect("livros:home1") #redirecionamento para a rota home
+    
+    formulario = {
+        "form":CadastroForm   #passando o formulario diretamente pro html
+    }
+    return render(request, 'crud/cadastrar.html', formulario)   #renderizar o conteúdo do arquivo cadastrar.html
