16ac39be757fb61f7adabb27819840735276e47b
LucasBaima <jhonelucasbay_@outlook.com>
2025-09-29 21:29:16 -0300

    Nova rota para Registrar o empréstimo do livro

--- livros/urls.py
+++ livros/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path("", views.home, name="home1"),
     path("cadastrar/", views.cadastrar_livro, name="cadastrar"), #Rota passada para o html dentro da tag
     path("remover/<int:id>", views.remover_livro, name="remover"),   #<int:id>  Informar qual item queremos remover .. id -> campo no banco de dados que representa o item
-    path("editar/<int:id>", views.editar_livro, name="editar") 
+    path("editar/<int:id>", views.editar_livro, name="editar"),
+    path("registrar/<int:id>", views.registrar_livro, name="registrar_livro") 
 ]
 
